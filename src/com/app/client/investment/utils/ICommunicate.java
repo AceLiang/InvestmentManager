6f48a86a7c44822a879d9b6fcaf643c25f19a24b
MattLeung <upside-down@live.com.cn>
2015-03-12 17:04:59 +0800

    Fragment Communicate Activity

---
+++ src/com/app/client/investment/utils/ICommunicate.java
@@ -0,0 +1,13 @@
+package com.app.client.investment.utils;
+import android.os.Message;
+/*****
+ * 
+ * @author Mattliang@apjcorp.com
+ * @date Mar 12, 2015 
+ * @version 1.0
+ * ËµÃ÷£º
+ */
+public interface ICommunicate {
+
+	public void	sendDataFromFragmentToActivity(Message data);
+}
