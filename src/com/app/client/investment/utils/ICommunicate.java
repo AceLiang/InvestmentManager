557781a29af802401716702288ea403235215a05
MattLeung <upside-down@live.com.cn>
2015-03-13 09:37:13 +0800

    11

--- src/com/app/client/investment/utils/ICommunicate.java
+++ src/com/app/client/investment/utils/ICommunicate.java
@@ -9,5 +9,21 @@ import android.os.Message;
  */
 public interface ICommunicate {
 
-	public void	sendDataFromFragmentToActivity(Message data);
+	/******
+	 * @author Mattliang@apjcorp.com
+	 * @version 1.0
+	 * @date Mar 13, 2015
+	 * 说明：
+	 * 1.activity 和 fragment 交互
+	 * 		activity实现该接口，当fragment需要passdata给activity的时候，
+	 * 		调用fragment.getActivity()，然后ICommunicate iInterface = (ICommunicate)activity;
+	 * 		iInterface.sendData();
+	 * 
+	 * 
+	 * 
+	 * 2.父fragment与子fragment交互（嵌套 Fragment ）
+	 * 		父fragment实现该接口，然后子fragment.getParentFragment，其它同上
+	 * @param data
+	 */
+	public void	sendData(Message data);
 }
