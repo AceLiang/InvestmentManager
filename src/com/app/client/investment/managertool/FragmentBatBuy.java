30eee15982e3101fa865bb84691ba735cbfc3910
AceLiang <upside-down@live.cn>
2015-03-20 23:03:00 +0800

    资本管理

--- src/com/app/client/investment/managertool/FragmentBatBuy.java
+++ src/com/app/client/investment/managertool/FragmentBatBuy.java
@@ -1,19 +1,28 @@
 package com.app.client.investment.managertool;
 
 import com.app.client.investment.R;
+import com.app.client.investment.utils.FakeListViewAdapter;
+import com.app.client.investment.utils.ViewUtils;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class FragmentBatBuy extends Fragment {
+	
+	ListView lvBatBuyInDetail ;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View root = inflater.inflate(R.layout.fragment_bat_buy, container, false) ;
+		lvBatBuyInDetail = (ListView) root.findViewById(R.id.lvBatBuyInDetail);
+		FakeListViewAdapter adapter = new FakeListViewAdapter(getActivity(), R.layout.item_batbuyin_detail, 100);
+		lvBatBuyInDetail.setAdapter(adapter);
+		ViewUtils.enableListViewinScrollerView(lvBatBuyInDetail);
 		return root;
 	}
 }
--- src/com/app/client/investment/managertool/FragmentSaleOut.java
+++ src/com/app/client/investment/managertool/FragmentSaleOut.java
@@ -1,19 +1,28 @@
 package com.app.client.investment.managertool;
 
 import com.app.client.investment.R;
+import com.app.client.investment.utils.FakeListViewAdapter;
+import com.app.client.investment.utils.ViewUtils;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class FragmentSaleOut extends Fragment {
+
+	ListView lvBuyInDetail ;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View root = inflater.inflate(R.layout.fragment_sale_out, container, false) ;
+		View root = inflater.inflate(R.layout.fragment_normal_buy, container, false) ;
+		lvBuyInDetail = (ListView) root.findViewById(R.id.lvBuyInDetail);
+		FakeListViewAdapter adapter = new FakeListViewAdapter(getActivity(), R.layout.listview_item_buyin_detal, 100);
+		lvBuyInDetail.setAdapter(adapter);
+		ViewUtils.enableListViewinScrollerView(lvBuyInDetail);
 		return root;
 	}
 }
