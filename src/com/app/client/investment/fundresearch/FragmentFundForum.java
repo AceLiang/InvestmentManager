a92f4a23fe3610ea320e2c25daffdca253441f97
anima <591151451@qq.com>
2015-03-21 12:07:41 +0800

    基金论坛

--- src/com/app/client/investment/fundresearch/FragmentFundForum.java
+++ src/com/app/client/investment/fundresearch/FragmentFundForum.java
@@ -5,18 +5,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.app.client.investment.R;
+import com.app.client.investment.utils.FakeListViewAdapter;
+import com.app.client.investment.utils.ViewUtils;
 /**
  * @author 简建鸿
  * 基金论坛
  */
 public class FragmentFundForum extends Fragment {
 
+	private ListView listView;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_fundresearch_fundforum, container, false);
+		
+		initView(rootView);
+		
 		return rootView;
+	}
+	
+	public void initView(View view) {
+		listView = (ListView) view.findViewById(R.id.fundresearch_fund_forum_ListView);
+		FakeListViewAdapter adapter = new FakeListViewAdapter(getActivity(), R.layout.listview_fund_form, 100);
+		listView.setAdapter(adapter);
+		ViewUtils.enableListViewinScrollerView(listView);
 	}
 }
