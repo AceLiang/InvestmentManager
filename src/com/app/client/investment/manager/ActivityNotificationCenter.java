1271534fe4646a8d345de4259b498f76c4a96219
AceLiang <upside-down@live.cn>
2015-03-24 23:21:52 +0800

    通知中心

--- src/com/app/client/investment/manager/ActivityNotificationCenter.java
+++ src/com/app/client/investment/manager/ActivityNotificationCenter.java
@@ -1,12 +1,17 @@
 package com.app.client.investment.manager;
 
 import com.app.client.investment.R;
+import com.app.client.investment.utils.FakeListViewAdapter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
-public class ActivityNotificationCenter extends Activity {
+public class ActivityNotificationCenter extends Activity implements OnItemClickListener{
 
 	
 	private ListView lv ;
@@ -18,5 +23,16 @@ public class ActivityNotificationCenter extends Activity {
 		setContentView(R.layout.activity_notification_center);
 		
 		lv = (ListView) findViewById(R.id.lv);
+		
+		FakeListViewAdapter adapter = new FakeListViewAdapter(this, R.layout.listview_notify_center, 100);
+		lv.setAdapter(adapter);
+		lv.setOnItemClickListener(this);
+	}
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		// TODO Auto-generated method stub
+		Intent intent = new Intent(this, ActivityNotificationDetail.class);
+		startActivity(intent);
 	}
 }
